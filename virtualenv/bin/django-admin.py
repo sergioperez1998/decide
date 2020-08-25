a2a818c9a7aac5252369428451a1d401bc3a5a64
sergioperez1998 <sergio-pm@hotmail.com>
2020-08-25 12:52:08 +0200

    configurando localsettings.py para que funcione travis

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jesus/Desktop/EGC/Practica Travis CI/decide/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
