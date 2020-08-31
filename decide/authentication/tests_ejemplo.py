12ed38bee04f2f4507e3781cc5d94f0743b62b15
sergioperez1998 <sergio-pm@hotmail.com>
2020-08-31 19:02:06 +0200

    probando credenciales

---
+++ decide/authentication/tests_ejemplo.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+from base import mods
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
