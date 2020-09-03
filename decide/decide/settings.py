c7c344e9964fa496600e617659a35a89c76bb6b4
sergioperez1998 <sergio-pm@hotmail.com>
2020-09-03 13:07:26 +0200

    Ignorando el local_settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -181,7 +181,8 @@ DEFAULT_VERSION = 'v1'
 try:
     from local_settings import *
 except ImportError:
-    print("local_settings.py not found")
+    #print("local_settings.py not found")
+    pass
 
 # loading jsonnet config
 if os.path.exists("config.jsonnet"):
